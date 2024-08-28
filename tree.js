b291d4caf4a1549e560ba28d506b7796bd81a95d
dustics4 <konrad.babowicz21@gmail.com>
2024-08-28 17:55:30 +0100

    added comments, fixed the root constructor of the tree class

--- tree.js
+++ tree.js
@@ -1,7 +1,23 @@
 import Node from "./node.js";
 
 class Tree{
-    constructor(){
-        this.root = Math.floor(new Array); // fix this
+    constructor(array){
+        this.root = this.buildTree(array); 
+    }
+
+    buildTree(array){
+        //create a sorted array ( use .. Set)
+
+        //create function build/ takes in arr
+        //base case - length of array === 0 return null
+        //calculate the mid
+        //let node = new Node(array[mid])
+
+        //node.left = // recursive function
+        //node.right = // recursive function
+
+        //return the node
+
+        //return build (sortedarray);
     }
 }
